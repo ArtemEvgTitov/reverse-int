233f2e579cf2980b673e48a4d34fa0e04257c514
ArtemEvgTitov <artem.titov.wg@gmail.com>
2024-07-29 14:02:15 +0300

    fix: fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+    let result = '';
+    number = String(Math.abs(number));
+    for (let i = number.length - 1; i >= 0; i--) {
+        result += number[i];
+    }
+    return Number(result);
 }
